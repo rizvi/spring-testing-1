a2417a212e08442aa61c8dd1f111900e84c3ce2f
Karl Nicholas <karl.nicholas@outlook.com>
2017-08-28 12:21:30 -0700

    updates run.cmd.

--- run.cmd
+++ run.cmd
@@ -1,4 +1,5 @@
 "c:\program files\curl\curl" -X POST localhost:80/shutdown
-timeout 5
+# ten second delay
+PING 127.0.0.1 -n 10 >NUL 2>&1 || PING ::1 -n 10 >NUL 2>&1
 copy target\spring-testing-1.5.6.RELEASE.jar c:\karl
 java -jar c:/karl/spring-testing-1.5.6.RELEASE.jar --server.port=80
